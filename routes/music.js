ce19e2b7d61373e4ac959173b7057f8e39be53fd
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-11 13:08:33 +0500

    added create new music api

--- routes/music.js
+++ routes/music.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+// MODEL
+const Music = require("../models/Music");
+
 // ADD MUSIC POST METHOD
-router.post("/", (req, res) => {
-  const { title } = req.body;
-  res.json(title)
+router.post("/create", (req, res) => {
+  // 1-WAY
+  // const { title, category, country, year, spotify_score } = req.body;
+  // const music = new Music({
+  //   title,
+  //   category,
+  //   country,
+  //   year,
+  //   spotify_score,
+  // });
+
+  const music = new Music(req.body);
+  music
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
