9e789d947087f745b0fa7ba94b449ee645a0c164
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-12 22:08:28 +0500

    added give all music api

--- routes/music.js
+++ routes/music.js
@@ -4,23 +4,38 @@ const router = express.Router();
 // MODEL
 const Music = require("../models/Music");
 
+// GET ALL MUSICS
+router.get("/all", async (req, res) => {
+  try {
+    const result = await Music.find();
+    res.json(result);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
 // ADD MUSIC POST METHOD
 router.post("/create", (req, res) => {
-  // 1-WAY
-  // const { title, category, country, year, spotify_score } = req.body;
-  // const music = new Music({
-  //   title,
-  //   category,
-  //   country,
-  //   year,
-  //   spotify_score,
-  // });
+  try {
+    // 1-WAY
+    // const { title, category, country, year, spotify_score } = req.body;
+    // const music = new Music({
+    //   title,
+    //   category,
+    //   country,
+    //   year,
+    //   spotify_score,
+    // });
 
-  const music = new Music(req.body);
-  music
-    .save()
-    .then((data) => res.json(data))
-    .catch((err) => res.json(err));
+    const music = new Music(req.body);
+    music
+      .save()
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
+
 
 module.exports = router;
